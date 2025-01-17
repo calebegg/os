efe5f598bde7523fe69d3b9331e89f3e1930e709
Caleb Eggensperger <calebegg@google.com>
2025-01-16 22:55:36 -0500

    Remove possibly unnecessary line

--- build.sh
+++ build.sh
@@ -4,6 +4,5 @@ set -ouex pipefail
 
 ### Install packages
 
-dnf install fedora-workstation-repositories dnf-plugins-core
-dnf config-manager --set-enabled google-chrome
+dnf install fedora-workstation-repositories
 dnf install -y fastfetch google-chrome-stable
