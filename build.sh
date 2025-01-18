1deefe26ded38afe2bc888acb74c4a1b89b34df8
Caleb Eggensperger <calebegg@google.com>
2025-01-17 21:12:31 -0500

    Add back chrome

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ set -ouex pipefail
 
 rsync -rvK /tmp/system_files/* /
 
-dnf install -y fastfetch distrobox vim tailscale
+dnf config-manager --set-enabled google-chrome
+dnf install -y fastfetch distrobox vim google-chrome-stable tailscale
 
 # Update gnome (dconf) settings from files copied into /etc/dconf/db/distro.d
 dconf update
