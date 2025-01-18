8f9e1cd8dd926945367b08567a9d8f19f2420849
Caleb Eggensperger <calebegg@google.com>
2025-01-17 20:55:11 -0500

    fix again

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ set -ouex pipefail
 
 rsync -rvK /tmp/system_files/* /
 
-dnf config-manager addrepo --from-repofile=/etc/yum.repos.d/*.repo
+dnf config-manager addrepo --from-repofile=/etc/yum.repos.d/google-chrome.repo
+dnf config-manager addrepo --from-repofile=/etc/yum.repos.d/tailscale.repo
 dnf install -y fastfetch distrobox vim google-chrome-stable tailscale
 
 # Update gnome (dconf) settings from files copied into /etc/dconf/db/distro.d
