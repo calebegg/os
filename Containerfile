16573c9cd842aeb92b06b70d837f96389dca1a08
Caleb Eggensperger <calebegg@google.com>
2025-01-17 19:44:07 -0500

    Progress

--- Containerfile
+++ Containerfile
@@ -1,5 +1,6 @@
 FROM quay.io/fedora/fedora-silverblue:latest
 
+COPY system_files /tmp/system_files
 COPY build.sh /tmp/build.sh
 RUN /tmp/build.sh
 
--- build.sh
+++ build.sh
@@ -2,7 +2,18 @@
 
 set -ouex pipefail
 
-dnf install -y fastfetch
+rsync -rvK /tmp/system_files /
+
+dnf install -y fastfetch distrobox vim google-chrome-stable tailscale
+
+# Update gnome (dconf) settings from files copied into /etc/dconf/db/distro.d
+dconf update
+
+curl --retry 3 -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
+tar -xzf /tmp/starship.tar.gz -C /tmp
+install -c -m 0755 /tmp/starship /usr/bin
+# shellcheck disable=SC2016
+echo 'eval "$(starship init bash)"' >> /etc/bashrc
 
 sed -i "s|^NAME=.*|NAME=\"calebegg-os\"|" /usr/lib/os-release
 sed -i "s|^ID=fedora|ID=calebegg-os\nID_LIKE=\"fedora\"|" /usr/lib/os-release
