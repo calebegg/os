d13d60ec3d355b1f0481fbd3c99cf7633bd055ab
Caleb Eggensperger <calebegg@google.com>
2025-01-17 22:58:34 -0500

    Add version # to os-release name

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ sed -i "s|^NAME=.*|NAME=\"calebegg-os\"|" /usr/lib/os-release
 sed -i "s|^ID=fedora|ID=calebegg-os\nID_LIKE=\"fedora\"|" /usr/lib/os-release
 sed -i "s|^HOME_URL=.*|HOME_URL=\"https://github.com/calebegg/os\"|" /usr/lib/os-release
 sed -i "s|^DEFAULT_HOSTNAME=.*|DEFAULT_HOSTNAME=\"cogsworth\"|" /usr/lib/os-release
-sed -i "s|^PRETTY_NAME=.*|PRETTY_NAME=\"calebegg's OS (from Fedora Silverblue)\"|" /usr/lib/os-release
+sed -i "s|^PRETTY_NAME=.*|PRETTY_NAME=\"calebegg's OS (from Fedora Silverblue $(rpm -E %fedora))\"|" /usr/lib/os-release
 sed -i "s|^ANSI_COLOR=.*|ANSI_COLOR=\"0;38;2;247;114;69\"|" /usr/lib/os-release
 
 sed -i "/^REDHAT/d;" /usr/lib/os-release
