40fc4c5699d883247a4ab062f987038706312cd5
Caleb Eggensperger <calebegg@google.com>
2025-01-19 22:48:02 -0500

    Refactor build script to modularize package installation and OS customization

--- build.sh
+++ build.sh
@@ -2,37 +2,67 @@
 
 set -ouex pipefail
 
-rsync -rvK /tmp/system_files/* /
+# Function to install required packages
+install_packages() {
+    dnf install -y \
+        fastfetch \
+        distrobox \
+        vim \
+        tailscale \
+        waydroid
 
-# dnf config-manager setopt google-chrome.enabled=1
-# google-chrome-stable
+    systemctl enable tailscaled
+}
 
-dnf install -y fastfetch distrobox vim tailscale waydroid
+# Function to install and configure starship prompt
+setup_starship() {
+    local STARSHIP_URL="https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
+    curl --retry 3 -Lo /tmp/starship.tar.gz "$STARSHIP_URL"
+    tar -xzf /tmp/starship.tar.gz -C /tmp
+    install -c -m 0755 /tmp/starship /usr/bin
+    # shellcheck disable=SC2016
+    echo 'eval "$(starship init bash)"' >> /etc/bashrc
+}
 
-systemctl enable tailscaled
+# Function to customize OS release information
+customize_os_release() {
+    local OS_RELEASE="/usr/lib/os-release"
+    local FEDORA_VERSION=$(rpm -E %fedora)
 
-# Update gnome (dconf) settings from files copied into /etc/dconf/db/distro.d
-dconf update
+    # Update OS information
+    sed -i -e "\
+        s|^NAME=.*|NAME=\"calebegg-os\"|;\
+        s|^ID=fedora|ID=calebegg-os\nID_LIKE=\"fedora\"|;\
+        s|^HOME_URL=.*|HOME_URL=\"https://github.com/calebegg/os\"|;\
+        s|^DEFAULT_HOSTNAME=.*|DEFAULT_HOSTNAME=\"cogsworth\"|;\
+        s|^PRETTY_NAME=.*|PRETTY_NAME=\"calebegg's OS (from Fedora Silverblue ${FEDORA_VERSION})\"|;\
+        s|^ANSI_COLOR=.*|ANSI_COLOR=\"0;38;2;247;114;69\"|" \
+        "$OS_RELEASE"
 
-curl --retry 3 -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
-tar -xzf /tmp/starship.tar.gz -C /tmp
-install -c -m 0755 /tmp/starship /usr/bin
-# shellcheck disable=SC2016
-echo 'eval "$(starship init bash)"' >> /etc/bashrc
+    # Remove unnecessary fields
+    sed -i -e "/^\(REDHAT\|CPE_NAME\|VARIANT\|VARIANT_ID\|DOCUMENTATION_URL\|SUPPORT_END\)=/d" "$OS_RELEASE"
+}
 
-sed -i "s|^NAME=.*|NAME=\"calebegg-os\"|" /usr/lib/os-release
-sed -i "s|^ID=fedora|ID=calebegg-os\nID_LIKE=\"fedora\"|" /usr/lib/os-release
-sed -i "s|^HOME_URL=.*|HOME_URL=\"https://github.com/calebegg/os\"|" /usr/lib/os-release
-sed -i "s|^DEFAULT_HOSTNAME=.*|DEFAULT_HOSTNAME=\"cogsworth\"|" /usr/lib/os-release
-sed -i "s|^PRETTY_NAME=.*|PRETTY_NAME=\"calebegg's OS (from Fedora Silverblue $(rpm -E %fedora))\"|" /usr/lib/os-release
-sed -i "s|^ANSI_COLOR=.*|ANSI_COLOR=\"0;38;2;247;114;69\"|" /usr/lib/os-release
+# Main execution
+main() {
+    # Copy system files
+    rsync -rvK /tmp/system_files/* /
 
-sed -i "/^REDHAT/d;" /usr/lib/os-release
-sed -i "/^CPE_NAME=/d;" /usr/lib/os-release
-sed -i "/^VARIANT=/d;" /usr/lib/os-release
-sed -i "/^VARIANT_ID=/d;" /usr/lib/os-release
-sed -i "/^DOCUMENTATION_URL=/d;" /usr/lib/os-release
-sed -i "/^SUPPORT_END=/d;" /usr/lib/os-release
+    # Install and configure packages
+    install_packages
 
-ostree container commit
-bootc container lint
+    # Update GNOME settings
+    dconf update
+
+    # Setup starship prompt
+    setup_starship
+
+    # Customize OS release information
+    customize_os_release
+
+    # Finalize container
+    ostree container commit
+    bootc container lint
+}
+
+main
